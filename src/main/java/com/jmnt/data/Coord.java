ee81d65db4f3c0b5a4bb0f72d0171d835944b158
Niilo Hautamäki <niilo.hautamaki@tuni.fi>
2024-10-18 15:47:08 +0300

    coordinate class

---
+++ src/main/java/com/jmnt/data/Coord.java
@@ -0,0 +1,19 @@
+package com.jmnt.data;
+
+public class Coord {
+    private double lat;
+    private double lon;
+
+    public Coord(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+}

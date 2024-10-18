9ec0240f4fe89d93024e7f0a79e6bad24a1b9abb
Joni Hämäläinen <joni.hamalainen@tuni.fi>
2024-10-18 14:39:18 +0300

    Fix file hiearchy

--- src/main/java/com/jmnt/application/Application.java
+++ src/main/java/com/jmnt/application/Application.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.jmnt.controllers",
+		"com.jmnt.configuration",
+		"com.jmnt.data",
+		"com.jmnt.excelparser"
+})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/jmnt/configuration/CorsConfig.java
+++ src/main/java/com/jmnt/configuration/CorsConfig.java
@@ -7,7 +7,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

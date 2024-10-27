c3fc7f550eeaa173f65318e2228e6bea52974957
Tenho Laakkio <tenho.laakkio@tuni.fi>
2024-10-27 10:58:40 +0200

    Added selecting field in university

--- frontend/src/components/Map/MapComponent.jsx
+++ frontend/src/components/Map/MapComponent.jsx
@@ -16,6 +16,8 @@ const MapComponent = ({type}) => {
     const [filteredData, setFilteredData] = React.useState([]);
     const [isPanelOpen, setIsPanelOpen]=useState(false);
     const [selectedSchool, setSelectedSchool] = useState(null);
+    const [selectedField,setSelectedField]=useState(null);
+    const [isFieldDataOpen,setIsFieldDataOpen]=useState(false);
     /*
     const allFields = Array.from(
         new Set(getData(type).flatMap((data) => data.fields))
@@ -61,36 +63,64 @@ const MapComponent = ({type}) => {
         setSelectedSchool(null);
     };
 
-    function SidePanel({school, closePanel,type}) {
+    const toggleFieldData = (field) => {
+        setSelectedField(field);
+        setIsFieldDataOpen(!isFieldDataOpen);
+    };
+
+    const closeFieldData = () => {
+        setIsFieldDataOpen(false);
+        setSelectedField(null);
+    };
+
+    function FieldData({field,closeFieldData}){
+        return (
+            <div className={`field-data-${field} ${isFieldDataOpen ? "field-data-open" : ""}`}>
+                <button onClick={closeFieldData} className="close-field-data-button">
+                    Close
+                </button>
+                <h4>Admission Methods:</h4>
+                <ul>
+                {field.admissionMethods?.map((method, methodIndex) => (
+                    <li key={methodIndex}>
+                        <p><strong>Method:</strong> {method.name}</p>
+                        <p><strong>Required Points:</strong> {method.requiredPoints}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+        </div>);
+    }
+
+    function SidePanel({school, closePanel, type}) {
         const [data, setData] = useState(null);
         const [loading, setLoading] = useState(true);
         const [error, setError] = useState(null);
 
         if (type === "high_school") {
             return (<div className={`side-panel ${isPanelOpen ? "side-panel-open" : ""}`}>
-                <button onClick={closePanel} className="close-panel-button">
-                    &times; Close
-                </button>
-                <h2>{school}</h2>
-            </div>
+                    <button onClick={closePanel} className="close-panel-button">
+                        &times; Close
+                    </button>
+                    <h2>{school}</h2>
+                </div>
             )
-        }
-        else {
-        useEffect(() => {
-            const fetchSchoolData = async () => {
-                try {
-                    setLoading(true);
-                    const result = await UniData(school); // Fetch school data based on name
-                    setData(result);
-                } catch (err) {
-                    setError("Error fetching school data");
-                } finally {
-                    setLoading(false);
-                }
-            };
+        } else {
+            useEffect(() => {
+                const fetchSchoolData = async () => {
+                    try {
+                        setLoading(true);
+                        const result = await UniData(school); // Fetch school data based on name
+                        setData(result);
+                    } catch (err) {
+                        setError("Error fetching school data");
+                    } finally {
+                        setLoading(false);
+                    }
+                };
 
-            if (school) fetchSchoolData();
-        }, [school]);
+                if (school) fetchSchoolData();
+            }, [school]);
 
         if (loading) return <div className="side-panel">Loading...</div>;
         if (error) return <div className="side-panel">Error: {error}</div>;
@@ -104,27 +134,30 @@ const MapComponent = ({type}) => {
 
                 {data && (
                     <>
-                        <p><strong>University Name:</strong>{data.name}</p>
+                        {selectedField === null ?
+                            (
+                                <div>
+                                    <p><strong>University Name:</strong>{data.name}</p>
+                                    <h3>Fields of Study:</h3>
+                                </div>
+                            ) :
+                            (
+                                <p><strong>Field:</strong> {selectedField.name}</p>
+                            )}
+
                         <div>
-                            <h3>Fields of Study:</h3>
+
                             <ul>
-                                {data.fields?.map((field, fieldIndex) => (
+                                {selectedField===null ?data.fields?.map((field, fieldIndex) => (
                                     <li key={fieldIndex}>
+                                        <Button onClick={() =>toggleFieldData(field)}>
                                         <p><strong>Field:</strong> {field.name}</p>
-                                        <div>
-                                            <h4>Admission Methods:</h4>
-                                            <ul>
-                                                {field.admissionMethods?.map((method, methodIndex) => (
-                                                    <li key={methodIndex}>
-                                                        <p><strong>Method:</strong> {method.name}</p>
-                                                        <p><strong>Required Points:</strong> {method.requiredPoints}
-                                                        </p>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </div>
+                                        </Button>
                                     </li>
-                                ))}
+                                ))
+                                : (<li>
+                                        <FieldData field={selectedField} closeFieldData={closeFieldData} />
+                                    </li>)}
                             </ul>
                         </div>
                     </>
